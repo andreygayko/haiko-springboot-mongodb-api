fe713b1809ba3f28cc2a3004f9d9c0f12a6320d9
unknown <andrey.gayko@gmail.com>
2020-06-24 20:57:03 +0300

    first commit

---
+++ src/main/java/com/haikospringbootmongodbapi/HaikoSpringbootMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.haikospringbootmongodbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaikoSpringbootMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaikoSpringbootMongodbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haikospringbootmongodbapi/HaikoSpringbootMongodbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haikospringbootmongodbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaikoSpringbootMongodbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
